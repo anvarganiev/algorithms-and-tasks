ab7b89c42faeb19e95547f2e3f6c2aac4aaa80dc
Anvar Ganiev <55971827+anvarganiev@users.noreply.github.com>
2021-02-01 11:25:23 +0300

    Create ex5.py

---
+++ olimpiade_exercises/ex5.py
@@ -0,0 +1,29 @@
+"""
+Дан массив из N целых чисел и некоторое число X.
+Найдите в массиве непустой подмассив (часть массива между некоторыми индексами l и r) минимальной длины такой, что сумма его элементов не менее X.
+"""
+print('Input N: ')
+N = int(input())
+
+arr = []
+print('Input array elements: ')
+arr = input().split()
+
+print('Enter X: ')
+X = int(input())
+
+lenght = []
+for i in range(len(arr) - 1):
+    temp = int(arr[i])
+    for j in range(i+1, len(arr)):
+        temp += int(arr[j])
+        if temp >= X:
+            lenght.append(j - i + 1)
+            break
+
+print(lenght)
+if not lenght:
+    print(-1)
+else:
+    mx = max(lenght)
+    print(mx)

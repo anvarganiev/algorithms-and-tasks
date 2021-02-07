96d2f1bd5d7d4b802c1c7d9187d4d94012d5c904
Anvar Ganiev <55971827+anvarganiev@users.noreply.github.com>
2021-02-07 13:25:17 +0300

    Update CourierEX2.py

--- Sirius_Yandex_Contest/Ex2/CourierEX2.py
+++ Sirius_Yandex_Contest/Ex2/CourierEX2.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 
 orders = []
 with open('input_ex2.txt') as input_file:
@@ -17,30 +16,3 @@ for order_id in range(amount):
         used_couriers += 1  # assign order to a new courier
     available_at[available_courier] = orders[order_id][1]  # update time of availability
 print(used_couriers)
-
-# orders = []
-# amount = 0
-# with open('input_ex2.txt') as input_file:
-#     amount = int(input_file.readline())
-#     for line in input_file.readlines():
-#         orders += [list(line.split())]
-#
-# available_at = ['00:00:00' for _ in range(amount)]
-#
-# orders = sorted(orders)
-#
-# used_couriers = 0
-#
-# for order_id in range(amount):
-#     available_courier = -1
-#     for courier_id in range(amount):
-#         if available_at[courier_id] <= orders[order_id][0]:
-#             available_courier = courier_id  # save available courier found
-#             break
-#
-#     if available_at[available_courier] == '00:00:00':
-#         used_couriers += 1 # assign order to a new courier
-#
-#     available_at[available_courier] = orders[order_id][1] # update time of availability
-#
-# print(used_couriers)
